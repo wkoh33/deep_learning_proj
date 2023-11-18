587564a3bb787d424090381661cfedf27689b54d
John <wkoh33@gatech.edu>
2023-11-18 11:04:17 -0500

    edit env var

--- set_env_variables.sh
+++ set_env_variables.sh
@@ -2,7 +2,7 @@
 # . set_env_variables.sh
 GATECH_EMAIL=wkoh33
 export ML_DATA=/home/hice1/$GATECH_EMAIL/scratch/data
-export PYTHONPATH=/home/hice1/$GATECH_EMAIL/dl_project
+export PYTHONPATH=/home/hice1/$GATECH_EMAIL/deep_learning_proj
 
 echo $ML_DATA
 echo $PYTHONPATH
